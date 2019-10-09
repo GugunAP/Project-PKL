a7d2fec9f83685dd4bf8688858f06e173fbf20e6
GugunAP <gunturadhip06@gmail.com>
2019-10-09 22:32:45 +0700

    initial commit

---
+++ app/src/main/java/com/example/projectpkl/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.projectpkl;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
