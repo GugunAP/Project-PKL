bbbd9e2f330033d2aeefbfe90d3035fe4a953bbd
GugunAP <gunturadhip06@gmail.com>
2019-11-04 23:25:37 +0700

    tak tambah i tab

--- app/src/main/java/com/example/projectpkl/HomeActivity.java
+++ app/src/main/java/com/example/projectpkl/HomeActivity.java
@@ -1,8 +1,14 @@
 package com.example.projectpkl;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,3 +18,12 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 }
+class TabHome extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_home,container,false);
+        return view;
+    }
+}
+
---
+++ app/src/main/java/com/example/projectpkl/MenuActivity.java
@@ -0,0 +1,18 @@
+package com.example.projectpkl;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MenuActivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_menu,container,false);
+        return view;
+    }
+}
